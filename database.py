0370f8b450330ef35b06b9ced0807c7a29420be4
GedeonGuercin <98447408+GedeonGuercin@users.noreply.github.com>
2022-10-13 23:48:59 -0400

    update

--- database.py
+++ database.py
@@ -6,3 +6,19 @@
 #-----------------------------------------------------------------------
 
 import sqlite3
+import contextlib
+
+#-----------------------------------------------------------------------
+
+_DATABASE_URL = 'file:data.sqlite?mode=ro'
+
+def get_name():
+
+    names = []
+    with sqlite3.connect(_DATABASE_URL, uri=True) as connection:
+
+        with contextlib.closing(connection.cursor()) as cursor:
+
+
+    return names
+
