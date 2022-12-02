07c2dc2108b0e5a5a152a5664206d60f31c19279
GedeonGuercin <98447408+GedeonGuercin@users.noreply.github.com>
2022-12-01 22:45:15 -0500

    Update AllThingsBlack.py

--- AllThingsBlack.py
+++ AllThingsBlack.py
@@ -75,7 +75,7 @@ def deleteresult():
 	database.delete_post(title)
 
 	message1 = 'The deletion was successful'
-	message2 = 'The database now does not contain a book with Title '
+	message2 = 'The database now does not contain a post with Title '
 	message2 += title
 
 	return report_results(message1, message2)
